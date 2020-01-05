f2ff2bcddabd9ae1a0e9d43f422414020e5a3014
mandrade <mandrade@bempromotora.com.br>
2020-01-05 11:50:21 -0300

    Correções temporárias

--- MandradePkgs.Retornos/ControllerApi.cs
+++ MandradePkgs.Retornos/ControllerApi.cs
@@ -4,30 +4,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MandradePkgs.Retornos
 {
-    public abstract class ControllerApi : ControllerBase
+    public class ControllerApi : ControllerBase
     {
         public ControllerApi() {
         }
 
-        public RespostaApi RespostaPadrao() {
+        protected RespostaApi RespostaPadrao() {
             var _mensagens = (MensagensApi)HttpContext.RequestServices.GetService(typeof(MensagensApi));
             Response.StatusCode = _mensagens.PossuiMensagensErro() ? 400 : 204;
             return new RespostaApi(_mensagens);
         }
 
-        public RespostaApi RespostaPadrao(bool sucesso) {
+        protected RespostaApi RespostaPadrao(bool sucesso) {
             var _mensagens = (MensagensApi)HttpContext.RequestServices.GetService(typeof(MensagensApi));
             Response.StatusCode = _mensagens.PossuiMensagensErro() ? 400 : 200;
             return new RespostaApi(sucesso, _mensagens);
         }
 
-        public RespostaApi<T> RespostaPadrao<T>(T dados) {
+        protected RespostaApi<T> RespostaPadrao<T>(T dados) {
             var _mensagens = (MensagensApi)HttpContext.RequestServices.GetService(typeof(MensagensApi));
             Response.StatusCode = _mensagens.PossuiMensagensErro() ? 400 : 200;
             return new RespostaApi<T>(dados, _mensagens);
         }
 
-        public RespostaApi<T> RespostaPadrao<T>(bool sucesso, T dados) {
+        protected RespostaApi<T> RespostaPadrao<T>(bool sucesso, T dados) {
             var _mensagens = (MensagensApi)HttpContext.RequestServices.GetService(typeof(MensagensApi));
             Response.StatusCode = _mensagens.PossuiMensagensErro() ? 400 : 200;
             return new RespostaApi<T>(sucesso, dados, _mensagens);
