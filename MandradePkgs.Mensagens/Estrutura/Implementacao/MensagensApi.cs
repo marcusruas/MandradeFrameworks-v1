fe20463f83edf143a558ad31cc7df41e99585202
mandrade <mandrade@bempromotora.com.br>
2020-01-08 23:04:07 -0300

    Corrigido pacote de mensagens.

--- MandradePkgs.Mensagens/Estrutura/Implementacao/MensagensApi.cs
+++ MandradePkgs.Mensagens/Estrutura/Implementacao/MensagensApi.cs
@@ -5,6 +5,11 @@ namespace MandradePkgs.Mensagens.Estrutura.Implementacao
 {
     internal class MensagensApi : IMensagensApi {
         public List<Mensagem> Mensagens { get; }
+
+        public MensagensApi() {
+            Mensagens = new List<Mensagem>();
+        }
+
         public void AdicionarMensagem(string mensagem) => Mensagens.Add(new Mensagem(mensagem));
         public void AdicionarMensagem(TipoMensagem tipoMensagem, string mensagem) => Mensagens.Add(new Mensagem(tipoMensagem, mensagem));
         public bool PossuiMensagensErro() => Mensagens.Any(x => x.Tipo == TipoMensagem.Erro.ToString());
