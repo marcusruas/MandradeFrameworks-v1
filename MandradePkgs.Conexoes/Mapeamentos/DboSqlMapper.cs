f0e783b61aa667162eeb9ad1c8c41c49b2a2ce36
mandrade <mandrade@bempromotora.com.br>
2020-01-14 23:28:40 -0300

    ...

--- MandradePkgs.Conexoes/Mapeamentos/DboSqlMapper.cs
+++ MandradePkgs.Conexoes/Mapeamentos/DboSqlMapper.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Reflection;
 
-namespace MandradePkgs.Mapeamentos
+namespace MandradePkgs.Conexoes.Mapeamentos
 {
     public static class DboSqlMapper
     {
