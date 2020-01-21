99338ed92fe20527a3e789965fd4de1503ca8130
mandrade <mandrade@bempromotora.com.br>
2020-01-21 20:49:10 -0300

    Correção do projeto de retorno.

--- MandradePkgs.Retornos/Estrutura/Filtros/ExceptionFiltro.cs
+++ MandradePkgs.Retornos/Estrutura/Filtros/ExceptionFiltro.cs
@@ -13,25 +13,31 @@ namespace MandradePkgs.Retornos.Estrutura.Filtros
             ApiException exceptionTratada;
             var _mensagens = (IMensagensApi)context.HttpContext.RequestServices.GetService(typeof(IMensagensApi));
 
-            if (context.Exception is FalhaExecucaoException) {
-                exceptionTratada = new FalhaExecucaoException(context.Exception.Message);
-                codigoResultado = 500;
-            }
-            else if (context.Exception is FalhaConexaoExternaException) {
-                exceptionTratada = new FalhaConexaoExternaException(context.Exception.Message);
-                codigoResultado = 503;
-            }
-            else if (context.Exception is FalhaConexaoException) {
-                exceptionTratada = new FalhaConexaoException(context.Exception.Message);
-                codigoResultado = 503;
-            }
-            else if (context.Exception is RegraNegocioException) {
-                exceptionTratada = new RegraNegocioException(context.Exception.Message);
-                codigoResultado = 400;
-            }
-            else {
+            if(context.Exception is ApiException) {
+                var msgTratada = ((ApiException)context.Exception).DescricaoErro;
+                if (context.Exception is FalhaExecucaoException) {
+                    exceptionTratada = new FalhaExecucaoException(msgTratada);
+                    codigoResultado = 500;
+                }
+                else if (context.Exception is FalhaConexaoExternaException) {
+                    exceptionTratada = new FalhaConexaoExternaException(msgTratada);
+                    codigoResultado = 503;
+                }
+                else if (context.Exception is FalhaConexaoException) {
+                    exceptionTratada = new FalhaConexaoException(msgTratada);
+                    codigoResultado = 503;
+                }
+                else if (context.Exception is RegraNegocioException) {
+                    exceptionTratada = new RegraNegocioException(msgTratada);
+                    codigoResultado = 400;
+                }
+                else {
+                    exceptionTratada = new GenericaException(msgTratada);
+                    codigoResultado = 422;
+                }
+            }else {
                 exceptionTratada = new GenericaException(context.Exception.Message);
-                codigoResultado = 422;
+                codigoResultado = 500;
             }
 
             ApiExceptionModel exceptionModel = new ApiExceptionModel(exceptionTratada);
