7d7ae1ea34d64ce88d7295818f4d6d65a0f1179d
mandrade <mandrade@bempromotora.com.br>
2020-01-25 20:58:03 -0300

    Incrementado pacote de retorno com mensagens.

--- MandradePkgs.Retornos/ControllerApi.cs
+++ MandradePkgs.Retornos/ControllerApi.cs
@@ -11,25 +11,25 @@ namespace MandradePkgs.Retornos
 
         protected RespostaApi RespostaPadrao() {
             var _mensagens = (IMensagensApi)HttpContext.RequestServices.GetService(typeof(IMensagensApi));
-            Response.StatusCode = _mensagens.PossuiMensagensErro() ? 400 : 204;
+            Response.StatusCode = _mensagens.PossuiMensagensErro() || _mensagens.PossuiFalhasValidacao() ? 400 : 204;
             return new RespostaApi(_mensagens.Mensagens);
         }
 
         protected RespostaApi RespostaPadrao(bool sucesso) {
             var _mensagens = (IMensagensApi)HttpContext.RequestServices.GetService(typeof(IMensagensApi));
-            Response.StatusCode = _mensagens.PossuiMensagensErro() ? 400 : 200;
+            Response.StatusCode = _mensagens.PossuiMensagensErro() || _mensagens.PossuiFalhasValidacao() ? 400 : 200;
             return new RespostaApi(sucesso, _mensagens.Mensagens);
         }
 
         protected RespostaApi<T> RespostaPadrao<T>(T dados) {
             var _mensagens = (IMensagensApi)HttpContext.RequestServices.GetService(typeof(IMensagensApi));
-            Response.StatusCode = _mensagens.PossuiMensagensErro() ? 400 : 200;
+            Response.StatusCode = _mensagens.PossuiMensagensErro() || _mensagens.PossuiFalhasValidacao() ? 400 : 200;
             return new RespostaApi<T>(dados, _mensagens.Mensagens);
         }
 
         protected RespostaApi<T> RespostaPadrao<T>(bool sucesso, T dados) {
             var _mensagens = (IMensagensApi)HttpContext.RequestServices.GetService(typeof(IMensagensApi));
-            Response.StatusCode = _mensagens.PossuiMensagensErro() ? 400 : 200;
+            Response.StatusCode = _mensagens.PossuiMensagensErro() || _mensagens.PossuiFalhasValidacao() ? 400 : 200;
             return new RespostaApi<T>(sucesso, dados, _mensagens.Mensagens);
         }
     }
